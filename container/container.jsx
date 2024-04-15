be24bee168d976de6b05f33babced3602cfca9d9
David Luciano Laureth da Silva <138811689+DavidLaurethAL@users.noreply.github.com>
2024-04-15 18:06:41 -0300

    Add files via upload

---
+++ container/container.jsx
@@ -0,0 +1,13 @@
+//import React from 'react';
+// import ReactDOM from 'react-dom';
+import './container.css'
+
+function Container() {
+  return (
+    <div className="container">
+      <div className="Laranja"></div>
+      <div className="Azul"></div>
+      <div className="AzulEscuro"></div>
+    </div>
+)}
+// ReactDOM.render(<Container />, document.getElementById('root'));
